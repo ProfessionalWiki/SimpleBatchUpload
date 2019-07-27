a513a0fbec907685ac4d42c029618d7256b5f657
Stephan <s7eph4n@protonmail.com>
2019-07-27 02:06:11 +0200

    Initial test setup

---
+++ tests/setup/install-mediawiki.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -ex
+
+wget https://github.com/wikimedia/mediawiki/archive/$MW.tar.gz
+tar -zxf $MW.tar.gz
+mv mediawiki-$MW mw
+
+cd mw
+
+composer install
+
+mysql -e 'create database wikidb;'
+php maintenance/install.php --dbuser root --dbname wikidb --pass hugo TestWiki admin
