9257544459df856f06c1bc8b7e27a092cb1d6d70
Stephan <s7eph4n@protonmail.com>
2019-08-10 15:51:00 +0200

    Put all testing into one shell script

--- tests/setup/run-tests.sh
+++ tests/setup/run-tests.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -ex
 
+cd ~
+
 wget https://github.com/wikimedia/mediawiki/archive/$MW.tar.gz
 tar -zxf $MW.tar.gz
 mv mediawiki-$MW mw
@@ -11,3 +13,8 @@ composer install
 
 mysql -e 'create database wikidb;'
 php maintenance/install.php --dbserver $SERVICE_MARIADB_IP --dbuser root --dbname wikidb --pass hugo TestWiki admin
+
+cp -R ~/build ~/mw/extensions/SimpleBatchUpload
+echo "wfLoadExtension( 'SimpleBatchUpload' );" >> ~/mw/LocalSettings.php
+
+php ~/mw/tests/phpunit/phpunit.php -c ~/mw/extensions/SimpleBatchUpload/phpunit.xml.dist --coverage-clover ~/coverage.xml
