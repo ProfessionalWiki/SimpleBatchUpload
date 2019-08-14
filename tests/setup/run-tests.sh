bccbc446d71fd351a6c20ee41062aeb2b82a00cc
Stephan <s7eph4n@protonmail.com>
2019-08-14 13:47:56 +0200

    Cleanup bash script

--- tests/setup/run-tests.sh
+++ tests/setup/run-tests.sh
@@ -1,20 +1,43 @@
 #!/bin/bash
 set -ex
 
-cd ~
+function fetch_mw_from_download() {
+    #TODO
+    echo Not implemented.
+}
 
-wget https://github.com/wikimedia/mediawiki/archive/$MW.tar.gz
-tar -zxf $MW.tar.gz
-mv mediawiki-$MW mw
+function fetch_mw_from_composer() {
 
-cd mw
+  wget https://github.com/wikimedia/mediawiki/archive/$MW.tar.gz
+  tar -zxf $MW.tar.gz
+  mv mediawiki-$MW ~/mw
 
-composer install
+  cd ~/mw
+  composer install
 
-mysql -e 'create database wikidb;'
-php maintenance/install.php --dbserver $SERVICE_MARIADB_IP --dbuser root --dbname wikidb --pass hugo TestWiki admin
+}
 
-cp -R ~/build ~/mw/extensions/SimpleBatchUpload
-echo "wfLoadExtension( 'SimpleBatchUpload' );" >> ~/mw/LocalSettings.php
+function fetch_sbu_from_download() {
+  cp -R ~/build ~/mw/extensions/SimpleBatchUpload
+}
 
-php ~/mw/tests/phpunit/phpunit.php -c ~/mw/extensions/SimpleBatchUpload/phpunit.xml.dist "$@"
+function fetch_sbu_from_composer() {
+  # TODO
+    echo Not implemented.
+}
+
+function install() {
+  mysql -e 'create database wikidb;'
+  php ~/mw/maintenance/install.php --dbserver $SERVICE_MARIADB_IP --dbuser root --dbname wikidb --pass hugo TestWiki admin
+  echo "wfLoadExtension( 'SimpleBatchUpload' );" >> ~/mw/LocalSettings.php
+}
+
+function run_tests() {
+  php ~/mw/tests/phpunit/phpunit.php -c ~/mw/extensions/SimpleBatchUpload/phpunit.xml.dist "$@"
+}
+
+fetch_mw_from_composer
+fetch_sbu_from_download
+install
+
+run_tests "$@"
