574a965665776390e7ab7d66980a8fbb1b906896
Morne Alberts <morne@malberts.net>
2022-12-14 18:35:37 +0200

    Update Promise methods

--- res/ext.SimpleBatchUpload.js
+++ res/ext.SimpleBatchUpload.js
@@ -83,7 +83,7 @@
 						filenode_text = ( dst_filename == src_filename ) ?
 							src_filename : `${src_filename} --> ${dst_filename}`;
 					}
-					
+
 					var status = $( '<li>' )
 					.attr( 'id', data.id )
 					.text( filenode_text )
@@ -117,7 +117,7 @@
 							};
 
 							data.submit()
-							.success( function ( result /*, textStatus, jqXHR */ ) {
+							.done( function ( result /*, textStatus, jqXHR */ ) {
 
 								if ( result.error !== undefined ) {
 
@@ -136,7 +136,7 @@
 								}
 
 							} )
-							.error( function ( /* jqXHR, textStatus, errorThrown */ ) {
+							.fail( function ( /* jqXHR, textStatus, errorThrown */ ) {
 								status.text( status.text() + " ERROR: Server communication failed." ).addClass( 'ful-error server-error' );
 								// console.log( JSON.stringify( arguments ) );
 							} );
